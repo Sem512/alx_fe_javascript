30c510eeba690dfb79b9bdade3231cd73213e99a
Semir Sultan Mohammed <semi512113@gmail.com>
2024-10-21 00:17:48 +0300

    modified script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -92,7 +92,7 @@
     ];
     
   
-    function fetchQuotesfromServer() {
+    function fetchQuotesFromServer() {
       return new Promise((resolve) => {
         setTimeout(() => {
           resolve(serverQuotes);
